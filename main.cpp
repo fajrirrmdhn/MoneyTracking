a3717fb907d8dd946bb57529acc580389f26eed6
Fajri <fajriramadhan71717@gmail.com>
2026-01-19 13:30:42 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "function.h"
+using namespace std;
 
+int main(){
+    int pilihan;
+
+    do{
+    tampilanAwal();
+
+    cout << "Masukkan Pilihan Anda: ";
+    cin >> pilihan;
+    cout << endl;
+
+    switch(pilihan) {
+        case 1:
+            pemasukanSaldo();
+            break;
+        case 2: 
+            pengeluaranSaldo();
+            break;
+        case 3:
+            cekSaldo();
+            break;
+        case 4:
+            riwayatTransaksi();
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        default:
+        cout << "Anda belum memilih" << endl;
+        }
+    }while(pilihan != 6);
+
+    return 0;
+}
